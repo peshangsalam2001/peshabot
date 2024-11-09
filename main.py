3d5629755ada8f73641c4f998ad0001c7112711a
peshangsalam2001 <peshangsalam2001@gmail.com>
2024-11-09 06:07:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import telebot
-from googletrans import Translator
+import yt_dlp
+import requests
+import time
 
-# Initialize the bot and translator
-bot_token = '7686120166:AAGnrPNFIHvgXdlL3G9inlouM3f7p7VZfkY'
-bot = telebot.TeleBot(bot_token)
-translator = Translator()
+API_TOKEN = '7686120166:AAGnrPNFIHvgXdlL3G9inlouM3f7p7VZfkY'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+def download_video(url):
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': '/tmp/%(title)s.%(ext)s',
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.download([url])
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict.get('title', None)
+        video_filename = ydl.prepare_filename(info_dict)
+        return video_filename
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Send me a video URL from Facebook, TikTok, Instagram, or Snapchat, and I'll download it for you!")
 
 @bot.message_handler(func=lambda message: True)
-def translate_message(message):
-    # Translate the incoming message from Kurdish to English
-    translated = translator.translate(message.text, src='ku', dest='en')
-    # Send the translated text back to the user
-    bot.send_message(message.chat.id, translated.text)
+def download_and_send_video(message):
+    url = message.text
+    try:
+        video_file = download_video(url)
+        with open(video_file, 'rb') as video:
+            bot.send_video(message.chat.id, video)
+        bot.send_message(message.chat.id, "Here is your video!")
+    except Exception as e:
+        bot.reply_to(message, "Sorry, I couldn't download the video. Please try again.")
+    time.sleep(5)  # Add a delay to prevent spamming
 
-# Start polling for messages
 bot.polling()
