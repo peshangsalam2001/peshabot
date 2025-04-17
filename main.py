2a42e499b0c331cbb4ae99e542640bbeaee255c7
peshangsalam2001 <peshangsalam2001@gmail.com>
2025-04-17 16:21:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import telebot
+from TikTokApi import TikTokApi
 
-# Token بۆ بۆتی تێلێگرامەکەت
-BOT_TOKEN = "7018443911:AAGuZfbkaQc-s2icbMpljkjokKkzg_azkYI"
-
-# دروستکردنی بۆت
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot("7018443911:AAGuZfbkaQc-s2icbMpljkjokKkzg_azkYI")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, " How are you سڵاو بەڕێز پێشەنگ")
+    bot.reply_to(message, "سڵاو! لینکی TikTok بنێرە بۆ داونلۆدکردن.")
 
-bot.remove_webhook()
+@bot.message_handler(func=lambda message: True)
+def download_tiktok(message):
+    url = message.text
+
+    try:
+        with TikTokApi() as api:
+            video = api.video(url=url)
+            video_data = video.bytes()
+
+        with open("video.mp4", "wb") as f:
+            f.write(video_data)
+
+        with open("video.mp4", "rb") as video:
+            bot.send_video(message.chat.id, video)
+
+    except Exception as e:
+        bot.reply_to(message, f"هەڵەیەک ڕوویدا:\n{e}")
 
 bot.polling()
