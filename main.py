cd1f671f856f9ee6879721a6a7f313876dc137a3
peshangsalam2001 <peshangsalam2001@gmail.com>
2025-04-17 16:12:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
 import telebot
-import requests
 
 # Token بۆ بۆتی تێلێگرامەکەت
-BOT_TOKEN = "7686120166:AAGnrPNFIHvgXdlL3G9inlouM3f7p7VZfkY"
+BOT_TOKEN = "7018443911:AAGuZfbkaQc-s2icbMpljkjokKkzg_azkYI"
 
 # دروستکردنی بۆت
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# API بۆ هەواڵ و زانیاری دراوەکان
-CURRENCY_API_URL = "https://api.exchangerate-api.com/v4/latest/USD"
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "سڵاو! من بۆتی دراوەکانم. بۆ وەرگرتنی نرخی دراوەکان، 'rate' بنووسە.")
+    bot.reply_to(message, "سڵاو بەڕێز پێشەنگ")
 
-@bot.message_handler(commands=['rate'])
-def send_currency_rate(message):
-    try:
-        response = requests.get(CURRENCY_API_URL)
-        data = response.json()
-        rates = data.get("rates", {})
+bot.remove_webhook()
 
-        msg = "نرخی نوێترین دراوەکان:\n"
-        msg += f"1 USD = {rates.get('EUR', 'N/A')} EUR\n"
-        msg += f"1 USD = {rates.get('GBP', 'N/A')} GBP\n"
-        msg += f"1 USD = {rates.get('IQD', 'N/A')} IQD\n"
-
-        bot.reply_to(message, msg)
-    except Exception as e:
-        bot.reply_to(message, "ببورە، ناتوانم زانیاری وەرگرم.")
-
-# ڕاپەچوو بۆ بۆت
 bot.polling()
