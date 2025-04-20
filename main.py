87605d1d52410609e4903ada1ccf1612075e4837
peshangsalam2001 <peshangsalam2001@gmail.com>
2025-04-20 22:40:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import telebot
 
+# زانیارییە بنەڕەتییەکان
 TOKEN = '7018443911:AAGuZfbkaQc-s2icbMpljkjokKkzg_azkYI'
 CHANNEL_LINK = 'https://t.me/PeshangTestChannel'
 ADMIN_CHAT_ID = 1908245207
 
 bot = telebot.TeleBot(TOKEN)
 
-# لیستی بەکارهێنەرانی کلیک‌کردو، لە یاداشتە ناوخۆیی
-clicked_users = set()
+# بۆ تۆمارکردنی ئەوەی تۆ کلیکت کردووە یان نا
+has_clicked = False
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -17,26 +18,24 @@ def send_welcome(message):
 
     bot.send_photo(
         message.chat.id,
-        photo='https://imgur.com/a/2EDWQ0H',  # گۆڕە بە لینکێکی وێنە
+        photo='https://example.com/image.jpg',  # گۆڕە بە لینکی وێنە
         caption="تکایە جۆینی ئەم چەناڵە بکەن",
         reply_markup=markup
     )
 
-    # نیشاندانی ID بۆ تۆ
     bot.send_message(message.chat.id, f"IDی تۆ: {message.chat.id}")
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call):
-    if call.data == 'joined':
-        user_id = call.from_user.id
-
-        if user_id not in clicked_users:
-            clicked_users.add(user_id)
-
-            if len(clicked_users) == 2:  # گەیشتن بە ٢ کلیک
-                bot.send_message(ADMIN_CHAT_ID, "پیرۆزە! ٢ بەکارهێنەر کلیکیان کرد و جۆینیان کردووە.")
+    global has_clicked
+    if call.data == 'joined' and call.from_user.id == ADMIN_CHAT_ID:
+        if not has_clicked:
+            has_clicked = True
+            bot.send_message(ADMIN_CHAT_ID, "پیرۆزە! تۆ کلیکی دوگمەکە کردووە.")
         else:
             bot.answer_callback_query(call.id, "تۆ پێشتر کلیکت کردووە.")
+    elif call.from_user.id != ADMIN_CHAT_ID:
+        bot.answer_callback_query(call.id, "تەنها خاونی بۆت دەتوانێ کلیک بکات.")
 
 # دەستپێکردنی بۆت
 bot.polling()
